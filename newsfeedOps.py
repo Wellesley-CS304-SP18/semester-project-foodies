d976fb4cffe5722c034024f4f68e5589302170de
minahattori <mhattori@wellesley.edu>
2018-05-17 19:23:57 -0400

    added comments in newsfeedOps.py

--- newsfeedOps.py
+++ newsfeedOps.py
@@ -23,14 +23,14 @@ def retrievePics(conn, username):
     return posts
 
 def getExplorePosts(conn):
-    '''Returns all of the pics from the people the user follows from the database in the form of a dictionary'''
+    '''Returns the top 30 posts (most liked) from all posts'''
     curs = conn.cursor(MySQLdb.cursors.DictCursor) # results as Dictionaries
     curs.execute('select posts.pic, posts.post_id,posts.username, posts.likes, posts.location, posts.description from posts order by posts.likes DESC limit 30')
     posts = curs.fetchall()
     return posts
 
 def updateLikes(conn, postid,username):
-    '''Returns the number of followers and number of other users the user is following'''
+    '''increases the number of likes by one when a new user likes a post, and updates that value in posts'''
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('insert into likes (post_id, username) values (%s, %s)',[postid,username])
     curs.execute('select COUNT(post_id) from likes where post_id=%s group by post_id' ,[postid])
@@ -39,6 +39,7 @@ def updateLikes(conn, postid,username):
     curs.execute('update posts set likes = %s where post_id = %s',[likes, postid])
 
 def updateUnlikes (conn, postid, username):
+    '''Decreses the number of likes by 1 when a user unlikes a post'''
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('delete from likes where post_id = %s and username = %s',[postid,username])
     curs.execute('select COUNT(post_id) from likes where post_id=%s group by post_id' ,[postid])
@@ -51,12 +52,14 @@ def updateUnlikes (conn, postid, username):
 
 
 def getnewLikes(conn, postid):
+    '''gets the new number of like'''
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('select likes from posts where post_id = %s',[postid])
     info = curs.fetchone()
     return info['likes']
 
 def checkLike(conn, username, postid):
+    ''''checks the number of likes given a username'''
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('select * from likes where post_id = %s and username = %s', [postid, username])
     if curs.fetchone():
@@ -64,11 +67,13 @@ def checkLike(conn, username, postid):
     return False
 
 def getComments(conn, postid):
+    '''gets comments for a post'''
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('select username, comment, time_stamp from comments where post_id = %s',[postid])
     return curs.fetchall()
 
 def addComment(conn, username, postid, comment, time_stamp):
+    '''Adds new comment to comments table'''
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('insert into comments (username, post_id, comment, time_stamp ) values (%s, %s, %s, %s)', [username, postid, comment, time_stamp])
 
